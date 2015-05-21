56d6fd96e49c3c3ed803d28c62aa586e49c2c672
Joe Buehl <joe@thebuehls.com>
2015-05-21 10:19:06 -0700

    cleanup

--- sql/emonth.sql
+++
@@ -1,8 +0,0 @@
-use solar;
-select 
-    day(day.date) Date, round(sum(day.eday)/1000, 3) Eday, round(avg(day.temp)*9/5+32, 0)
-    from (select date, id, max(eday) eday, temp
-        from inverters
-        where month(date) = month(now()) 
-        group by date, id) day 
-    group by day.date;
--- sql/load.sql
+++
@@ -1,3 +0,0 @@
-use solar;
-load data local infile '/root/solar/inv.csv' into table inverters fields terminated by ',';
-load data local infile '/root/solar/opt.csv' into table optimizers fields terminated by ',';
