b251da1a9f0a7e7e648acdefc7051afcf83d4629
Joe Buehl <joe@thebuehls.com>
2016-02-03 11:15:56 -0800

    Add master mode

--- semonitor.py
+++ semonitor.py
@@ -2,8 +2,8 @@
 
 # Monitor SolarEdge inverters using the SolarEdge protocol on the RS485 interface
 
-# Usage: semonitor [-a] [-c capfile] [-D delimiter] [-f] [-H] [-i invfile] [-o optfile] 
-#                  [-v] inFile
+# Usage: semonitor [-a] [-b baudrate] [-D delimiter] [-f] [-H] [-i invfile] [-o optfile] 
+#                  [-v] inFile [outFile]
 # Arguments:
 #   inFile          file or serial port to read
 #                   If a serial port is specified, monitor the data in real time.
@@ -13,7 +13,7 @@
 #                   further data to be written to the file.
 # Options:
 #   -a              append to output files
-#   -c capfile      file to capture data to
+#   -b              baud rate for serial input (default: 115200)
 #   -D delim        output file delimiter (default: ",")
 #   -f              output appended data as the pcap file grows (as in tail -f)
 #   -H              write column headers to output files
@@ -27,7 +27,6 @@
 # Examples:
 
 import os
-import socket
 import struct
 import sys
 import time
@@ -35,6 +34,8 @@ import getopt
 import syslog
 import json
 import serial
+import threading
+#Simport crc16
 
 # configuration
 debug = False
@@ -45,23 +46,29 @@ debugRaw = False
 debugSeq = []
 inFileName = ""
 inputSeq = 0
-capFileName = ""
+outFileName = ""
 invFileName = ""
 optFileName = ""
 jsonFileName = ""
 headers = False
 delim = ","
 writeMode = "w"
-master = False
+masterMode = False
 sleepInterval = 10
 dbRetryInterval = 60
 logSysout = False
 lineSize = 16
 appName = "semonitor"
+serialIn = False
+baudRate = 115200
+masterThreadName = "master"
+masterMsgInterval = 1
+waiting = True
+ignore = [0x0302, 0x0347, 0x039a, 0x039f]
 
 # file handles
 inFile = None
-capFile = None
+outFile = None
 invFile = None
 optFile = None
 jsonFile = None
@@ -163,14 +170,19 @@ def invDictData(seInvData):
             }
 
 def readFile():
+    global waiting
     # skip to the start of the first record
     readRec()
     inputSeq = 1
     while True:
         rec = readRec()
+#        waiting = False
         if True: #len(rec) > 100:
-            if debugRaw: dumpRec(rec)
-            convertRec(rec, inputSeq)
+            try:
+                convertRec(rec, inputSeq)
+            except:
+                log("exception***")
+                dumpRec(rec)
         inputSeq += 1
 
 # return the next record in the file
@@ -189,9 +201,9 @@ def readBytes(length):
     while inBuf == None:
         time.sleep(sleepInterval)
         inBuf = inFile.read(length)
-    if capFile:
-        capFile.write(inBuf)
-        capFile.flush()
+    if outFile:
+        outFile.write(inBuf)
+        outFile.flush()
     return inBuf
 
 # hex dump the specified record
@@ -205,34 +217,44 @@ def dumpRec(rec):
 
 # hex dump a line of data
 def dumpLine(rec):
-    log(appName, "rawdata:", ' '.join(x.encode('hex') for x in rec))
+    log("rawdata:", ' '.join(x.encode('hex') for x in rec))
 
 # formatted print a record header
 def printRecHdr(seDataLen, ffff, msgSeq, invFrom, invTo, function):
-    log(appName, "dataLen:  ", "%04x" % seDataLen)
-    log(appName, "filler:   ", "%04x" % ffff)
-    log(appName, "sequence: ", "%04x" % msgSeq)
-    log(appName, "source:   ", "%08x" % invFrom)
-    log(appName, "dest:     ", "%08x" % invTo)
-    log(appName, "function: ", "%04x" % function)
+    log("dataLen:  ", "%04x" % seDataLen)
+    log("flags:    ", "%04x" % ffff)
+    log("sequence: ", "%04x" % msgSeq)
+    log("source:   ", "%08x" % invFrom)
+    log("dest:     ", "%08x" % invTo)
+    log("function: ", "%04x" % function)
 
 # formatted print a device header
 def printDevHdr(seType, seId, seDeviceLen):
-    log(appName, "    type:     ", "%04x" % seType)
-    log(appName, "    id:       ", "%08x" % seId)
-    log(appName, "    len:      ", "%04x" % seDeviceLen)
+    log("    type:     ", "%04x" % seType)
+    log("    id:       ", "%08x" % seId)
+    log("    len:      ", "%04x" % seDeviceLen)
 
 # formatted print device data
 def printDevice(devData):
     for field in devData.keys():
-        log(appName, "   ", field, ":", devData[field])
-            
+        log("   ", field, ":", devData[field])
+
+# convert a data record            
 def convertRec(rec, inputSeq):
-    if debugData: log(appName, "record:", inputSeq, "length:", len(rec))
+    global waiting
     # record header
     (magic) = struct.unpack(">L", rec[0:4])
-    if debugData: log(appName, "magic:    ", "%08x" % magic)
     (seDataLen, ffff, msgSeq, invFrom, invTo, function) = struct.unpack("<HHHLLH", rec[4:20])
+#    if function in ignore:
+#        return
+#    if inputSeq not in range(35350, 35359):
+#        return
+#    if inputSeq > 100:
+#        sys.exit(0)
+    if debugRecs: log(" ")
+    if debugRaw: dumpRec(rec)
+    if debugData: log("record:", inputSeq, "length:", len(rec))
+    if debugData: log("magic:    ", "%08x" % magic)
     if debugData: printRecHdr(seDataLen, ffff, msgSeq, invFrom, invTo, function)
     dataPtr = 20
     while dataPtr < len(rec)-4:
@@ -242,7 +264,7 @@ def convertRec(rec, inputSeq):
             dataPtr += seDevHdrLen
             # device data
             if seType == 0x0000:    # optimizer log data
-                if debugData: log(appName, "optimizer:     ")
+                if debugData: log("optimizer:     ")
                 if debugData: printDevHdr(seType, seId, seDeviceLen)
                 seOptData = list(struct.unpack(optInFmt, rec[dataPtr:dataPtr+seDeviceLen]))
     #            seOptData[2] = convertId(seOptData[2])
@@ -257,7 +279,7 @@ def convertRec(rec, inputSeq):
     #            writeJson()
     #            writeData(seId, seOptData, optIdx, optOutFmt, optFile)
             elif seType == 0x0010:  # inverter log data
-                if debugData: log(appName, "inverter:     ")
+                if debugData: log("inverter:     ")
                 if debugData: printDevHdr(seType, seId, seDeviceLen)
                 seInvData = list(struct.unpack(invInFmt, rec[dataPtr:dataPtr+seDeviceLen]))
                 invDict[seId] = invDictData(seInvData)
@@ -265,16 +287,82 @@ def convertRec(rec, inputSeq):
                 writeJson()
                 writeData(seId, seInvData, invIdx, invOutFmt, invFile)
             else:   # unknown device type
-                if debugData: log(appName, "unknown:     ")
+                if debugData: log("unknown:     ")
                 dumpRec(rec[dataPtr:dataPtr+seDeviceLen])
             dataPtr += seDeviceLen
         else:
             dumpRec(rec[dataPtr:-2])
             dataPtr = len(rec)-2
-    if debugData: log(appName, "checksum: ", "%04x" % struct.unpack("<H", rec[-2:]))
-#    if debugData: log(appName, "checksum: ", "%04x" % checksum16(rec[4:-2]))
-    if debugData: log(appName)
+    if debugData: log("checksum: ", "%04x" % struct.unpack("<H", rec[-2:]))
+#    checkRec = rec[4:-2]
+#    if debugData: log("mod16:    ", "%04x" % checksum16(checkRec))
+#    if debugData: log("CRC16:    ", "%04x" % crc16.crc16xmodem(checkRec))
+#    if debugData: log("IP:       ", "%04x" % IPChecksum(checkRec))
+#    if debugData: log("fletcher: ", "%04x" % fletcher(checkRec))
+#    if debugData: log("BSD:      ", "%04x" % bsdChecksum(checkRec))
+    if debugData: log(" ")
 
+# Decode optimiser data in packet type 0x0080
+#  (into same order as original data)
+#
+# Byte index (in reverse order):
+# 
+# 0c 0b 0a 09 08 07 06 05 04 03 02 01 00
+# Tt Ee ee Cc cO o# pp Uu uu Dd dd dd dd 
+#  # = oo|Pp
+#
+#  Temp, 8bit (1.6 degC)  Signed?, 1.6 is best guess at factor
+#  Energy in day, 16bit (1/4 Wh)
+#  Current (panel), 12 bit (1/160 Amp)
+#  voltage Output, 10 bit (1/8 v)
+#  voltage Panel, 10 bit (1/8 v)
+#  Uptime of optimiser, 16 bit (secs)
+#  DateTime, 32 bit (secs)
+#
+def convertNewOptData(seData):
+    data = bytearray()
+    data.extend(seData)
+    (timeStamp, uptime) = struct.unpack("<LH", seData[0:6])
+    vpan = 0.125 * (data[6] | (data[7] <<8 & 0x300))
+    vopt = 0.125 * (data[7] >>2 | (data[8] <<6 & 0x3c0))
+    imod = 0.00625 * (data[9] <<4 | (data[8] >>4 & 0xf))
+    eday = 0.25 * (data[11] <<8 | data[10])
+    temp = 1.6 * struct.unpack("<b", seData[12:13])[0]
+    # Don't have an inverter ID in the data, substitute 0
+    return [timeStamp, 0, 0, uptime, vpan, vopt, imod, eday, temp]
+
+# compute BSD checksum
+def bsdChecksum(msg):
+    checksum = 0
+    for ch in msg:
+        checksum = (checksum >> 1) + ((checksum & 1) << 15)
+        checksum += ord(ch)
+        checksum &= 0xffff;
+    return checksum
+
+# compute IP checksum
+def IPChecksum(msg):
+    s = 0
+    for i in range(0, len(msg), 2):
+        w = ord(msg[i]) + (ord(msg[i+1]) << 8)
+        s = carry_around_add(s, w)
+    return ~s & 0xffff
+
+def carry_around_add(a, b):
+    c = a + b
+    return (c & 0xffff) + (c >> 16)
+
+# compute Fletcher checksum
+def fletcher(msg):
+    sum1 = 0
+    sum2 = 0
+    for i in range(len(msg)):
+        sum1 = ( sum1 + ord(msg[i]) ) % 255
+        sum2 = ( sum2 + sum1       ) % 255
+    check1 = 255 - (( sum1 + sum2    ) % 255)
+    check2 = 255 - (( sum1 + check1 ) % 255)
+    return (check1 << 8) + check2
+    
 # Compute the 16 bit checksum of a string of bytes               
 def checksum16(msg):
     return reduce(lambda x,y:x+y, map(ord, msg)) % 16384
@@ -286,7 +374,7 @@ def convertId(seId):
 # write device data to json file
 def writeJson():
     if jsonFileName != "":
-#        if debugFiles: log("writing", jsonFileName)
+        if debugRecs: log("writing", jsonFileName)
         json.dump({"inverters": invDict, "optimizers": optDict}, open(jsonFileName, "w"))
     
 # write device data
@@ -294,8 +382,9 @@ def writeData(seId, seData, seIdx, outFmt, outFile):
     if outFile:
         outRec = outFmt % outData(seId, seData, seIdx)
         try:
-            outFile.write(outRec+"\n")
             if debugRecs: log("writing", outRec)
+            outFile.write(outRec+"\n")
+            outFile.flush()
         except:
             terminate(1, "Error writing output file "+outFile.name)
 
@@ -317,19 +406,23 @@ def printTime(timeStamp):
 # get command line options and arguments
 def getOpts():
     global debug, debugFiles, debugRecs, debugData, debugRaw
-    global writeMode, delim, follow, headers, master
-    global capFileName, invFileName, optFileName, jsonFileName
+    global writeMode, delim, follow, headers, masterMode, baudRate
+    global outFileName, invFileName, optFileName, jsonFileName
     global pcapDir, inFileName, inFiles, logSysout
-    (opts, args) = getopt.getopt(sys.argv[1:], "ac:D:fp:Hi:j:lmo:v")
+    (opts, args) = getopt.getopt(sys.argv[1:], "ab:D:fp:Hi:j:lmo:v")
     try:
         inFileName = args[0]
     except:
         inFileName = "stdin"      
+    try:
+        outFileName = args[1]
+    except:
+        pass
     for opt in opts:
         if opt[0] == "-a":
             writeMode = "a"
-        elif opt[0] == "-c":
-            capFileName = opt[1] 
+        elif opt[0] == "-b":
+            baudRate = opt[1] 
         elif opt[0] == "-D":
             delim = opt[1] 
         elif opt[0] == "-f":
@@ -343,7 +436,7 @@ def getOpts():
         elif opt[0] == "-l":
             logSysout = True
         elif opt[0] == "-m":
-            master = True
+            masterMode = True
         elif opt[0] == "-o":
             optFileName = opt[1]
         elif opt[0] == "-v":
@@ -363,23 +456,24 @@ def getOpts():
         log("headers:", headers)
         log("delim:", delim)
         log("append:", writeMode)
+        log("baudRate:", baudRate)
         log("inFileName:", inFileName)
-        log("capFileName:", capFileName)
+        log("outFileName:", outFileName)
         log("invFileName:", invFileName)
         log("optFileName:", optFileName)
         log("jsonFileName:", jsonFileName)
         log("logSysout:", logSysout)
-        log("master:", master)
+        log("masterMode:", masterMode)
 
 # open the output files if they are specified
 def openOutFiles():
-    global capFile, invFile, optFile
-    if capFileName != "":
+    global outFile, invFile, optFile
+    if outFileName != "":
         try:
-            if debugFiles: log("writing", capFileName)
-            capFile = open(capFileName, writeMode)
+            if debugFiles: log("writing", outFileName)
+            outFile = open(outFileName, writeMode)
         except:
-            terminate(1, "Unable to open "+capFileName)
+            terminate(1, "Unable to open "+outFileName)
     if invFileName != "":
         try:
             if debugFiles: log("writing", invFileName)
@@ -394,26 +488,69 @@ def openOutFiles():
             if headers: optFile.write(optHdr+"\n")
         except:
             terminate(1, "Unable to open "+optFileName)
+    if jsonFileName != "":
+        if debugFiles: log("writing", jsonFileName)
 
 # open the specified input file
 def openInFile(inFileName):
-    global inFile, inputSeq
+    global inFile, inputSeq, serialIn
     if inFileName == "stdin":
         inFile = sys.stdin
     elif inFileName[0:8] == "/dev/tty":
-        inFile = serial.Serial(inFileName, baudrate=115200)
+        serialIn = True
+        inFile = serial.Serial(inFileName, baudrate=baudRate)
     else:
         try:
             if debugFiles: log("opening", inFileName)
             inFile = open(inFileName)
             inputSeq = 0
+            if masterMode:
+                terminate(1, "Master mode not allowed with file input")
         except:
             terminate(1, "Unable to open "+inFileName)
 
+# start a thread to send RS485 master messages
+def startMaster():
+    if masterMode:
+        masterThread = threading.Thread(name=masterThreadName, target=sendMasterMsg)
+        masterThread.start()
+        if debugFiles: log("starting", masterThreadName)
+        
+# master message thread
+def sendMasterMsg():
+#    msg = "\x12\x34\x56\x79\x00\x00\xff\xff\x9c\x6e\x20\x49\x10\x7f\x16\x4a\x10\x7f\x02\x03\xf5\xa6"
+    chksum = 0x3bbb
+    while waiting:
+        msg = struct.pack("<LHHHLLHH", 0x79563412, 0x0000, 0xffff, 0x0000, 0x7f100000, 0x7f104a16, 0x0302, chksum)
+        if debugData: 
+            log(masterThreadName, "sending")
+            dumpRec(msg)
+            log(" ")
+        inFile.write(msg)
+        time.sleep(masterMsgInterval)
+        
+# master message thread
+#def sendMasterMsg():
+##    msg = "\x12\x34\x56\x79\x00\x00\xff\xff\x9c\x6e\x20\x49\x10\x7f\x16\x4a\x10\x7f\x02\x03\xf5\xa6"
+#    chksum = 0x3bb0
+#    masterMsgInterval = 1
+#    while waiting:
+#        msg = struct.pack("<LHHHLLHH", 0x79563412, 0x0000, 0xffff, 0x0000, 0x7f100000, 0x7f104a16, 0x0302, chksum)
+#        if chksum % 0x0100 == 0:
+#            log("trying", "0x%04x"%chksum)
+#        if debugData: 
+#            log(masterThreadName, "sending")
+#            dumpRec(msg)
+#            log(" ")
+#        inFile.write(msg)
+#        time.sleep(masterMsgInterval)
+#        chksum += 1
+#    log("match", "0x%04x"%chksum)
+        
 # close all files        
 def closeFiles():
     if inFile: inFile.close()
-    if capFile: capFile.close()
+    if outFile: outFile.close()
     if invFile: invFile.close()
     if optFile: optFile.close()
 
@@ -430,9 +567,9 @@ def log(*args):
     for arg in args[1:]:
         message += arg.__str__()+" "
     if logSysout:
-        print message
+        print time.asctime(time.localtime())+" "+message
     else:
-        syslog.syslog(message)
+        syslog.syslog(appName+" "+message)
 
 def terminate(code, msg=""):
     print msg
@@ -447,5 +584,9 @@ if __name__ == "__main__":
     if debugFiles: log("reading", inFileName)
     openInFile(inFileName)
     openOutFiles()
-    readFile()
-    closeFiles()
+    try:
+        startMaster()
+        readFile()
+    except KeyboardInterrupt:
+        waiting = False
+        closeFiles()
