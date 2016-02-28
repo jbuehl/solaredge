520ee035c104fba02dc4fe8da529be521ba8bd91
Joe Buehl <joe@thebuehls.com>
2016-02-28 14:29:49 -0800

    rearrange parameters

--- seConf.py
+++ seConf.py
@@ -6,16 +6,28 @@ import time
 import getopt
 import sys
 import socket
+import netifaces
 
-# debugging
+# debug flags
 debugEnable = True
 debugFiles = False
 debugMsgs = False
 debugData = False
 debugRaw = False
+logStdout = False
+haltOnException = False
 
-# config defaults
+# data input parameters
 inFileName = ""
+following = False
+baudRate = 115200
+masterMode = False
+slaveAddrs = []
+restart = False
+networkMode = False
+serialDevice = False
+
+# output file parameters
 outFileName = ""
 invFileName = ""
 optFileName = ""
@@ -23,34 +35,31 @@ jsonFileName = ""
 headers = False
 delim = ","
 writeMode = "w"
-following = False
-haltOnException = False
-baudRate = 115200
-masterMode = False
-slaveAddrs = []
-networkMode = False
-logStdout = False
 
-# global variables and constants
+# global constants
 bufSize = 1024
 parsing = True
-waiting = True
 sleepInterval = .1
 lineSize = 16
 appName = "semonitor"
 serialFileName = "/dev/tty"
-masterThreadName = "master"
+readThreadName = "read thread"
+masterThreadName = "master thread"
 masterMsgInterval = 5
 masterAddr = 0xfffffffe
 
 # network constants
-thisIpAddr = socket.inet_aton("0.0.0.0")
+netInterface = ""
+ipAddr = ""
+broadcastAddr = ""
+subnetMask = ""
 sePort = 22222
 dhcpDnsBufferSize = 4096
-dhcpLeaseTime = 24*60*60
-validMac = "\xb8\x27\xeb"
-validMac = "\x00\x27\x02"
-dnsTtl = 24*60*60
+dhcpLeaseTime = 24*60*60    # 1 day
+validMacs = ["\xb8\x27\xeb",   # Raspberry Pi
+             "\x00\x27\x02",   # SolarEdge
+             ]
+dnsTtl = 24*60*60           # 1 day
 
 # log a message
 def log(*args):
@@ -97,11 +106,16 @@ def logData(data):
             logLine(data[printPtr:])
 
 # get program arguments and options
-(opts, args) = getopt.getopt(sys.argv[1:], "ab:D:fp:Hi:j:lmn:o:s:vx")
+(opts, args) = getopt.getopt(sys.argv[1:], "ab:D:fp:Hi:j:lmn:o:rs:vx")
 try:
     inFileName = args[0]
+    if inFileName == "-":
+        inFileName = "stdin"
+    if inFileName[0:len(serialFileName)] == serialFileName:
+        serialDevice = True      
 except:
-    inFileName = "stdin"      
+    networkMode = True
+    inFileName = "network"
 try:
     outFileName = args[1]
 except:
@@ -127,9 +141,19 @@ for opt in opts:
         masterMode = True
     elif opt[0] == "-n":
         networkMode = True
-        thisIpAddr = socket.inet_aton(opt[1])
+        try:
+            # network interface parameters
+            netInterface = opt[1]
+            netInterfaceParams = netifaces.ifaddresses(netInterface)[2][0]
+            ipAddr = netInterfaceParams["addr"]
+            broadcastAddr = netInterfaceParams["broadcast"]
+            subnetMask = netInterfaceParams["netmask"]
+        except:
+            terminate(1, "network interface is not available")
     elif opt[0] == "-o":
         optFileName = opt[1]
+    elif opt[0] == "-r":
+        restart = True
     elif opt[0] == "-s":
         slaveAddrs = opt[1].split(",")
     elif opt[0] == "-v":
@@ -145,38 +169,50 @@ for opt in opts:
     elif opt[0] == "-x":
         haltOnException = True
 
-# force following for input from stdin or serial device
-if (inFileName == "stdin") or (inFileName[0:len(serialFileName)] == serialFileName):
+# force following for input from serial device
+if serialDevice:
     following = True
-
-# determine if data is going to need to be parsed
-if (outFileName != "") and (invFileName == "") and (optFileName == "") and (jsonFileName == ""):
-    parsing = False
 
 # network mode implies master mode
 if networkMode:
     masterMode = True
-    
+elif masterMode and not serialDevice:
+    terminate(1, "Master mode not allowed with file input")
+   
+# determine if data is going to need to be parsed
+if (outFileName != "") and (invFileName == "") and (optFileName == "") and (jsonFileName == "") and not masterMode:
+    parsing = False
+
 if debugFiles: 
     log("debugEnable:", debugEnable)  
     log("debugFiles:", debugFiles)  
     log("debugMsgs:", debugMsgs)
     log("debugData:", debugData)
     log("debugRaw:", debugRaw)
-    log("headers:", headers)
-    log("delim:", delim)
-    log("append:", writeMode)
-    log("baudRate:", baudRate)
+    log("logStdout:", logStdout)
+    log("haltOnException:", haltOnException)
     log("following:", following)
+    log("parsing:", parsing)
     log("inFileName:", inFileName)
+    log("serialDevice:", serialDevice)
+    if serialDevice:
+        log("baudRate:", baudRate)
+    log("restart:", restart)
+    log("masterMode:", masterMode)
+    if masterMode:
+        log("slaveAddrs:", slaveAddrs)
+    log("networkMode:", networkMode)
+    if netInterface != "":
+        log("netInterface", netInterface)
+        log("ipAddr", ipAddr)
+        log("subnetMask", subnetMask)
+        log("broadcastAddr", broadcastAddr)
     log("outFileName:", outFileName)
     log("invFileName:", invFileName)
     log("optFileName:", optFileName)
+    if (invFileName != "") or (optFileName != ""):
+        log("headers:", headers)
+        log("delim:", delim)
+    log("append:", writeMode)
     log("jsonFileName:", jsonFileName)
-    log("logStdout:", logStdout)
-    log("masterMode:", masterMode)
-    log("slaveAddrs:", slaveAddrs)
-    log("haltOnException:", haltOnException)
-    log("networkMode:", networkMode)
-    log("thisIpAddr:", socket.inet_ntoa(thisIpAddr))
 
