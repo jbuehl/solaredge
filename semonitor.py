941edb503085edbcb3ecf70cf7c57e3caff76aea
Joe Buehl <joe@thebuehls.com>
2016-03-11 20:27:32 -0800

    Add documentation

--- semonitor.py
+++ semonitor.py
@@ -16,25 +16,25 @@
 #   outFile          Optional file to copy all incoming and outgoing messages to
 
 # Options:
-#   -a               append to inverter, optimizer, json, and output files
-#   -b               baud rate for serial input (default: 115200)
-#   -c cmd[/cmd/...] send the specified command sequence
-#   -D delim         inverter and optimizer file delimiter (default: ",")
-#   -f               output appended data as the input file grows (as in tail -f)
-#   -H               write column headers to inverter and optimizer files
-#   -i invfile       file to write inverter data to
-#   -j jsonfile      json file to write data to
-#   -l               send log messages to stdout
-#   -m               function as a RS485 master
-#   -n interface     run network services on the specified interface
-#   -o optfile       file to write optimizer data to
-#   -s invdddrs      comma delimited list of SolarEdge slave inverter addresses
-#   -v               verbose output
-#   -x               halt on data exception
+#   -a                  append to inverter, optimizer, json, and output files
+#   -b                  baud rate for serial input (default: 115200)
+#   -c cmd[/cmd/...]    send the specified command functions
+#   -D delim            inverter and optimizer file delimiter (default: ",")
+#   -f                  output appended data as the input file grows (as in tail -f)
+#   -H                  write column headers to inverter and optimizer files
+#   -i invfile          file to write inverter data to
+#   -j jsonfile         json file to write data to
+#   -l                  send log messages to stdout
+#   -m                  function as a RS485 master
+#   -n interface        run network services on the specified interface
+#   -o optfile          file to write optimizer data to
+#   -s invid[,invid...] comma delimited list of SolarEdge slave inverter IDs
+#   -v                  verbose output
+#   -x                  halt on data exception
 
 # Notes:
 #    Data may be read from a file containing messages in the SolarEdge protocol that was previously created by 
-#    seextract.py from a pcap file, or the output from a previous run of semonitor.py.  It may also be
+#    seextract from a pcap file, or the output from a previous run of semonitor.  It may also be
 #    read in real time from one of the RS232, RS485, or ethernet interfaces on a SolarEdge inverter.
 #
 #    Messages are sent to the system log, unless the -l option is specified.  If an error occurs
@@ -44,15 +44,28 @@
 #    The level of debug messaging is controlled using the -v option, which may be specified up to
 #    4 times:
 #        -v      log input parameters and file operations
-#        -vv     log incoming and outgoing messages and their sizes
+#        -vv     log incoming and outgoing messages
 #        -vvv    log the parsed data of incoming and outgoing messages
 #        -vvvv   log the raw data of incoming and outgoing messages
+#    Messages logged at the -vv level and above contain the device or file sending or receiving the
+#    message, the direction it was sent, the message size, and an internal sequence number.  Separate
+#    sequences are kept for incoming and outgoing messages.
+#
+#    To interact directly with an inverter over the network, semonitor must function as the SolarEdge
+#    monitoring server.  This means that the host running semonitor must be connected to the inverter
+#    over a dedicated ethernet connection.  In this configuration, semonitor functions as the DHCP server
+#    and the DNS server to provide an IP address to the inverter and to resolve the hostname of the
+#    SolarEdge server (usually prod.solaredge.com) to the IP address of the semonitor host.  This
+#    requires that semonitor be run with elevated (root) priveleges in order to access the standard
+#    DHCP and DNS ports.  The -n option specifies the name of the network interface that the inverter
+#    is connected to.
 #
 #    The -c, -m, and -s options are not meaningful if input is from a file or stdin.
 #
-#    The -m option is only valid if a serial port is specified and one or more inverter serial
-#    numbers must be specified with the -s option.  If this option is specified, there cannot
-#    be another master device on the RS485 bus.
+#    The -m option is only valid if a serial port is specified, and one or more inverter IDs
+#    must be specified with the -s option.  If this option is specified, there cannot
+#    be another master device on the RS485 bus.  semonintor will repeatedly send commands to
+#    the specified inverters to request performance data.
 #
 #    The -c option may be specified for a serial device or the network.  The option specifies one
 #    or more SolarEdge protocol command functions separated by a "/".  Each command function
@@ -63,6 +76,10 @@
 #        H = 16 bits
 #        L = 32 bits
 #    All function codes and parameters must be hexadecimal numbers, without the leading "0x".
+#    Exactly one inverter ID must be specified with the -s option.  After each command is sent,
+#    semonitor will wait for a response before sending the next command.  When all commands
+#    have been sent and responded to, the program terminates.  Use the -vvv option to view
+#    the responses.
 
 # Examples:
 #    # python semonitor.py -i yyyymmdd.inv -o yyyymmdd.opt -H yyyymmdd.se
@@ -76,23 +93,24 @@
 #
 #    # python seextract.py yyyymmdd.pcap | python semonitor.py -j yyyymmdd.json
 #
-#    Extract SolarEdge data from the file yyyymmdd.pcap and write data to the json file 
-#    yyyymmdd.json.
+#    Extract SolarEdge data from the file yyyymmdd.pcap using seextract, process
+#    it with semonitor, and write data to the json file yyyymmdd.json.
 #
 #    # python semonitor.py -j yyyymmdd.json -m -s 7f101234,7f105678 COM4
 #
 #    Function as a RS485 master to request data from the inverters 7f101234 and 7f105678
 #    using serial port COM4.
 #
-#    # python semonitor.py -c 0012,H0329 -s 7f101234 /dev/ttyUSB0
+#    # python semonitor.py -c 0012,H0329 -s 7f101234 -l -vvv /dev/ttyUSB0
 #
 #    Send a command to the inverter 7f101234 to request the value of parameter 0x0329
-#    using serial port /dev/ttyUSB0.
+#    using serial port /dev/ttyUSB0.  Display the messages on stdout.
 #
-#    # python semonitor.py -c 0011,H329,L1/0012,H329/0030,H01f4,L0 -s 7f101234 /dev/ttyUSB0
+#    # python semonitor.py -c 0011,H329,L1/0012,H329/0030,H01f4,L0 -s 7f101234 -l -vvv /dev/ttyUSB0
 #
 #    Send commands to the inverter 7f101234 to set the value of parameter 0x0329 to 1,
 #    followed by a command to reset the inverter using serial port /dev/ttyUSB0.
+#    Display the messages on stdout.
 #
 #    # sudo python semonitor.py -j yyyymmdd.json -n eth1
 #
