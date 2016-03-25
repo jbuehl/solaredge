32dabf07500c4fe3984377a7a95f6d34685d81b2
Joe Buehl <joe@thebuehls.com>
2016-03-24 18:24:21 -0700

    Initialize state from file.  Zero optimizer on event.

--- se2state.py
+++ se2state.py
@@ -7,9 +7,6 @@ import getopt
 import time
 import sys
 
-# state values
-stateDict = {"inverters": {}, "optimizers": {}}
-
 # get program arguments and options
 (opts, args) = getopt.getopt(sys.argv[1:], "o:")
 try:
@@ -19,6 +16,13 @@ except:
 for opt in opts:
     if opt[0] == "-o":
         outFileName = opt[1]
+        
+# initialize the state from the file if it exists
+try:
+    with open(outFileName) as stateFile:
+        stateDict = json.load(stateFile)
+except:
+    stateDict = {"inverters": {}, "optimizers": {}}
         
 # read the input forever
 while True:
@@ -36,6 +40,10 @@ while True:
         for inverter in stateDict["inverters"].keys():
             stateDict["inverters"][inverter]["Eac"] = 0.0
             stateDict["inverters"][inverter]["Pac"] = 0.0
+        for optimizer in stateDict["optimizers"].keys():
+            stateDict["optimizers"][optimizer]["Vmod"] = 0.0
+            stateDict["optimizers"][optimizer]["Vopt"] = 0.0
+            stateDict["optimizers"][optimizer]["Imod"] = 0.0
     # update the state file
     with open(outFileName, "w") as outFile:
         json.dump(stateDict, outFile)
