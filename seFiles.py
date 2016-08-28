2c564c12987a626ec684f03cdc1dc6117b265e10
Joe Buehl <joe@thebuehls.com>
2016-08-28 15:59:40 -0700

    Add comments for listening on multiple ports

--- seFiles.py
+++ seFiles.py
@@ -6,6 +6,26 @@ import socket
 from seConf import *
 from seNetwork import *
 
+#servers = [] 
+
+#for port in portlist:
+#    ds = ("0.0.0.0", port)
+
+#    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#    server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+#    server.bind(ds)
+#    server.listen(1)
+
+#    servers.append(server)
+
+#while True:
+#    # Wait for any of the listening servers to get a client
+#    # connection attempt
+#    readable,_,_ = select.select(servers, [], [])
+#    ready_server = readable[0]
+
+#    connection, address = ready_server.accept()
+    
 # open data socket and wait for connection from inverter
 def openDataSocket():
     try:
