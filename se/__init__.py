3aeffa20885818db5a54bd6a43b3a30fa99920a2
jbuehl <joe@thebuehls.com>
2017-11-25 16:09:02 -0800

    Jb rename (#67)
    * Rename modules and move to se/ library to be more pep8 compliant
    
    * Rename modules and move to se/ library to be more pep8 compliant
    
    * Rename modules and move to se/ library to be more pep8 compliant

--- se/__init__.py
+++
@@ -1,9 +0,0 @@
-from commands import *
-from data import *
-from datadevices import *
-from dataparams import *
-from files import *
-from logging import *
-from msg import *
-from network import *
-
