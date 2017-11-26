bab986d8ed73c455f10c1c49670ad94e40a733e5
Eric Buehl <eric.buehl@gmail.com>
2017-11-25 17:40:08 -0800

    add comment to unhexlify

--- utilities/unhexlify.py
+++ utilities/unhexlify.py
@@ -2,6 +2,11 @@
 
 """
 used for decoding streams of hex such as those outputted by tshark
+
+the encoding of stream data from tshark is normally necessary to distinguish which side of the conversation
+a given messages is going to/from.  However, since the solaredge protocol is based on an RS485 (bus) protocol
+all information about the source and destination addresses are encapsulated in the higher-level solaredge
+protocol.
 """
 
 import binascii
