d2284df36eeffbc4c1c54d7843869b7cbf942e89
Olliver Schinagl <oliver@schinagl.nl>
2018-01-17 21:22:53 +0100

    test: make variables safer
    Overal it is recommended to use the ${VAR} syntax over plain $VAR.
    Further more, it is also recommended to quote variables containing
    strings.
    
    So lets make our variables a little bit safer.
    
    Signed-off-by: Olliver Schinagl <oliver@schinagl.nl>

--- test/test.sh
+++ test/test.sh
@@ -12,15 +12,15 @@ do
 
     SAMPLE="${pcap%.*}"
     KEY_OPTION=""
-    if [ -f test/keys/$SAMPLE.key ]
+    if [ -f "test/keys/${SAMPLE}.key" ]
     then
-        KEY_OPTION="-k test/keys/$SAMPLE.key"
+        KEY_OPTION="-k test/keys/${SAMPLE}.key"
     fi
-    echo Running test sample: $SAMPLE
-    echo Key option: $KEY_OPTION
-    diff <(tshark -r test/pcap/$SAMPLE.pcap -T fields -e data | ./utilities/unhexlify.py | ./semonitor.py - $KEY_OPTION) test/json/$SAMPLE.json
-    cat test/json/$SAMPLE.json | conversion/se2csv.py -p $TMP/$SAMPLE -t
-    diff $TMP/ test/csv/$SAMPLE/ -w
+    echo "Running test sample: ${SAMPLE}"
+    echo "Key option: ${KEY_OPTION}"
+    diff <(tshark -r "test/pcap/${SAMPLE}.pcap" -T fields -e data | ./utilities/unhexlify.py | ./semonitor.py - ${KEY_OPTION}) "test/json/${SAMPLE}.json"
+    cat "test/json/${SAMPLE}.json" | conversion/se2csv.py -p "${TMP}/${SAMPLE}" -t
+    diff "${TMP}/" "test/csv/${SAMPLE}/" -w
 
     rm -rf "${TMP}"
 done
