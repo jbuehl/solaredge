96ab8ba49e11c821a09d98cffac72d3fe0e46511
Olliver Schinagl <oliver@schinagl.nl>
2018-01-18 21:29:23 +0100

    Test improvements (#75)
    * test: use reliable temporary directory creation
    
    Making random directories in /tmp is frowned uppon and generally
    considered not wise. The recommended tool for this job is mktemp.
    
    Change the test script and use mktemp rather then manually creating
    temporary directories.
    
    Signed-off-by: Olliver Schinagl <oliver@schinagl.nl>
    
    * test: make variables safer
    
    Overal it is recommended to use the ${VAR} syntax over plain $VAR.
    Further more, it is also recommended to quote variables containing
    strings.
    
    So lets make our variables a little bit safer.
    
    Signed-off-by: Olliver Schinagl <oliver@schinagl.nl>
    
    * test: store se2csv test results in its own subdirectory
    
    To ensure we do not get strange clashes or diff errors while comparing
    all files in a directory, make sure that only this specific test sets
    data is in a single directory.
    
    Signed-off-by: Olliver Schinagl <oliver@schinagl.nl>
    
    * test: test the -o flag
    
    Store the result using the output parameter code path, which is slightly
    more likley to go wrong then stdout.
    
    Signed-off-by: Olliver Schinagl <oliver@schinagl.nl>
    
    * test: also test for the -r parameter
    
    To get a little wider test coverage, let us test the record output
    parameter as well.
    
    Signed-off-by: Olliver Schinagl <oliver@schinagl.nl>

--- test/test.sh
+++ test/test.sh
@@ -2,22 +2,28 @@
 
 export TZ='US/Pacific'
 
-TMP=/tmp/outputs
-
 for pcap in `ls test/pcap`
 do
-    rm -rf $TMP
-    mkdir $TMP
+    TMP=$(mktemp -d)
+    if [ -z "${TMP}" ]; then
+        echo "Failed to create temporary directory '${TMP}'"
+        break
+    fi
+    TMPSE2CSV="${TMP}/se2csv"
 
     SAMPLE="${pcap%.*}"
     KEY_OPTION=""
-    if [ -f test/keys/$SAMPLE.key ]
+    if [ -f "test/keys/${SAMPLE}.key" ]
     then
-        KEY_OPTION="-k test/keys/$SAMPLE.key"
+        KEY_OPTION="-k test/keys/${SAMPLE}.key"
     fi
-    echo Running test sample: $SAMPLE
-    echo Key option: $KEY_OPTION
-    diff <(tshark -r test/pcap/$SAMPLE.pcap -T fields -e data | ./utilities/unhexlify.py | ./semonitor.py - $KEY_OPTION) test/json/$SAMPLE.json
-    cat test/json/$SAMPLE.json | conversion/se2csv.py -p $TMP/$SAMPLE -t
-    diff $TMP/ test/csv/$SAMPLE/ -w
+    echo "Running test sample: ${SAMPLE}"
+    echo "Key option: ${KEY_OPTION}"
+    tshark -r "test/pcap/${SAMPLE}.pcap" -T fields -e data | ./utilities/unhexlify.py | ./semonitor.py -r "${TMP}/${SAMPLE}.rec" -o "${TMP}/${SAMPLE}.json" - $KEY_OPTION
+    diff "${TMP}/${SAMPLE}.json" "test/json/${SAMPLE}.json"
+    mkdir "${TMPSE2CSV}"
+    cat "test/json/${SAMPLE}.json" | conversion/se2csv.py -p "${TMPSE2CSV}/${SAMPLE}" -t
+    diff "${TMP}/se2csv" "test/csv/${SAMPLE}/" -w
+
+    rm -rf "${TMP}"
 done
