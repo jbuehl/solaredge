471ef19b5a35f0671e2b44be9ca1a769d4782807
Olliver Schinagl <o.schinagl@ultimaker.com>
2018-07-22 06:17:59 +0200

    Increase testing verbosity (#88)

--- test/test.sh
+++ test/test.sh
@@ -1,30 +1,34 @@
-#!/bin/bash -e
+#!/usr/bin/env bash
+
+set -o pipefail
+set -eux
 
 export TZ='US/Pacific'
 
-for pcap in `ls test/pcap`
-do
+for pcap in test/pcap/*.pcap; do
     TMP=$(mktemp -d)
-    if [ -z "${TMP}" ]; then
+    if [ ! -d "${TMP}" ]; then
         echo "Failed to create temporary directory '${TMP}'"
         break
     fi
     TMPSE2CSV="${TMP}/se2csv"
-
-    SAMPLE="${pcap%.*}"
+    SAMPLE="$(basename --suffix=".pcap" "${pcap}")"
+    REC_OPTION="-r ${TMP}/${SAMPLE}.rec"
+    OUT_OPTION="-o ${TMP}/${SAMPLE}.json"
     KEY_OPTION=""
-    if [ -f "test/keys/${SAMPLE}.key" ]
-    then
+    if [ -f "test/keys/${SAMPLE}.key" ]; then
         KEY_OPTION="-k test/keys/${SAMPLE}.key"
     fi
-    echo "Running test sample: ${SAMPLE}"
-    echo "Key option: ${KEY_OPTION}"
-    tshark -r "test/pcap/${SAMPLE}.pcap" -T fields -e data | ./utilities/unhexlify.py | ./semonitor.py -r "${TMP}/${SAMPLE}.rec" -o "${TMP}/${SAMPLE}.json" - $KEY_OPTION
+    SEMONITOR_OPTIONS="${REC_OPTION} ${OUT_OPTION} - ${KEY_OPTION}"
+
+    tshark -r "test/pcap/${SAMPLE}.pcap" -T fields -e data | ./utilities/unhexlify.py | ./semonitor.py ${SEMONITOR_OPTIONS}
     diff "${TMP}/${SAMPLE}.json" "test/json/${SAMPLE}.json"
     cmp -l "${TMP}/${SAMPLE}.rec" "test/rec/${SAMPLE}.rec"
     mkdir "${TMPSE2CSV}"
-    cat "test/json/${SAMPLE}.json" | conversion/se2csv.py -p "${TMPSE2CSV}/${SAMPLE}" -t
-    diff "${TMP}/se2csv" "test/csv/${SAMPLE}/" -w
+    ./conversion/se2csv.py -p "${TMPSE2CSV}/${SAMPLE}" -t < "test/json/${SAMPLE}.json"
+    diff "${TMPSE2CSV}/" "test/csv/${SAMPLE}/" -w
 
-    rm -rf "${TMP}"
+    if [ -d "${TMP}" ] && [ "${TMP#/tmp/tmp.}x" != "${TMP}x" ]; then
+        rm -rf "${TMP}"
+    fi
 done
