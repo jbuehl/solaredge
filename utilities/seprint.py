1de49e01b9330799e76c508707d65869aea82cbb
Joe Buehl <joe@thebuehls.com>
2019-06-01 09:59:19 -0700

    Use python3 print

--- utilities/seprint.py
+++ utilities/seprint.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-# print selected data from a JSON file
+# print(selected data from a JSON file
 
 import json
 import sys
@@ -16,18 +16,18 @@ jsonStr = inFile.readline()
 while jsonStr != "":
     inDict = json.loads(jsonStr)
     if inDict["inverters"] != {}:
-        print "Inverters"
+        print("Inverters")
         for inverter in sorted(inDict["inverters"].keys()):
             invDict = inDict["inverters"][inverter]
-            print "  ", invDict["Date"], invDict["Time"], inverter, "Eday:%9.2f"%invDict["Eday"], "Pac:%7.2f"%invDict["Pac"], "Vac:%6.2f"%invDict["Vac"]
+            print("  "+invDict["Date"]+" "+invDict["Time"]+" "+inverter+" "+"Eday:%9.2f"%invDict["Eday"]+" "+"Pac:%7.2f"%invDict["Pac"]+" "+"Vac:%6.2f"%invDict["Vac"])
     if inDict["optimizers"] != {}:
-        print "Optimizers"
+        print("Optimizers")
         for optimizer in sorted(inDict["optimizers"].keys()):
             optDict = inDict["optimizers"][optimizer]
-            print "  ", optDict["Date"], optDict["Time"], optimizer, "Eday:%7.2f"%optDict["Eday"]
+            print("  "+optDict["Date"]+" "+optDict["Time"]+" "+optimizer+" "+"Eday:%7.2f"%optDict["Eday"])
     if inDict["events"] != {}:
-        print "Events"
+        print("Events")
         for event in sorted(inDict["events"].keys()):
             eventDict = inDict["events"][event]
-            print "  ", eventDict["Date"], eventDict["Time"], event
+            print("  "+eventDict["Date"]+" "+eventDict["Time"]+" "+event)
     jsonStr = inFile.readline()
